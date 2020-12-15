597c2389da6e54b2bf2a29433b212e074b913fa3
Manh Hung <manhhung99@Manhs-MacBook-Pro.local>
2020-12-15 16:08:22 +0700

    init app file-upload-donetcore

---
+++ Controllers/FileUploadController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpPost("FileUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files)
+        {
+                long size = files.Sum(f => f.Length);
+
+            var filePaths = new List<string>();
+            foreach (var formFile in files)
+            {
+                if (formFile.Length > 0)
+                {
+                    var fileExName = Path.GetExtension(formFile.FileName);
+                    var fileName = GetFileName(DateTime.Now, fileExName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"public/earthquake_data", fileName);
+
+                    filePaths.Add(filePath);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await formFile.CopyToAsync(stream);
+                    }
+                }
+            }
+
+            return Ok(new { countFile = files.Count, status= "success" });
+        }
+
+        public string GetFileName(DateTime date, string exName)
+        {
+            var day = date.Day > 9 ? date.Day.ToString() : $"0{date.Day}";
+            var month = date.Month > 9 ? date.Month.ToString() : $"0{date.Month}";
+
+            return $"{day}{month}{date.Year}{exName}";
+        }
+
+    }
+}
